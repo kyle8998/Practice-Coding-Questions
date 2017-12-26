5ec104996bf848b59d538787b2aebad21e106137
Kyle Lim <kylelim8998@gmail.com>
2017-12-25 23:03:35 -0500

    Leetcode 29-Medium-Divide-Two-Integers
    Python Solution

---
+++ leetcode/29-Medium-Divide-Two-Integers/answer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+#------------------------------------------------------------------------------
+
+class Solution:
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        positive = (dividend < 0) is (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        res = 0
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                res += i
+                i <<= 1
+                temp <<= 1
+        if not positive:
+            res = -res
+        return min(max(-2147483648, res), 2147483647)
+        
+
+#------------------------------------------------------------------------------
+#Testing
---
+++ leetcode/29-Medium-Divide-Two-Integers/problem.md
@@ -0,0 +1,18 @@
+# Problem 29: Divide Two Integers
+
+
+#### Difficulty: Medium
+
+#### Problem
+
+Divide two integers without using multiplication, division and mod operator.
+
+If it is overflow, return MAX_INT.
+
+#### Example
+
+<pre>
+
+Division
+
+</pre>
